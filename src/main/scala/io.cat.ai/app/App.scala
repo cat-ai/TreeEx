a3ae6bb521ad906b19318f75cfcc96fc41305185
Gadzhiev Kemran <k.gadzhiev@pflb.ru>
2019-07-06 20:42:03 +0300

    little fix

--- src/main/scala/io.cat.ai/app/App.scala
+++ src/main/scala/io.cat.ai/app/App.scala
@@ -3,19 +3,19 @@ package io.cat.ai.app
 import java.io.File
 
 import io.cat.ai.app.console.args.TreeExArgs
-import io.cat.ai.app.processor.{Processor, TreeExFileProcessor}
+import io.cat.ai.app.processor.TreeExFileProcessor
 import io.cat.ai.app.util.TreeExOptions
 import io.cat.ai.core.graph.Graph
 
-trait App[F[_]] extends TreeExApp[F, Seq[String], TreeExArgs, TreeExOptions, Graph[File], Processor[File, String]] {
+trait App[F[_]] extends TreeExApp[F, Seq[String], TreeExArgs, TreeExOptions, Graph[File], TreeExFileProcessor] {
 
   def parseArgs(args: Seq[String]): F[TreeExArgs]
 
   def buildOptions(treeExArgs: TreeExArgs): F[TreeExOptions]
 
-  def createGraph(treeExPathMode: TreeExOptions): F[Graph[File]]
+  def createGraph(options: TreeExOptions): F[Graph[File]]
 
-  def createProcessor(treeExMode: TreeExOptions): F[TreeExFileProcessor]
+  def createProcessor(options: TreeExOptions): F[TreeExFileProcessor]
 }
 
 object App {
--- src/main/scala/io.cat.ai/app/TreeExApp.scala
+++ src/main/scala/io.cat.ai/app/TreeExApp.scala
@@ -1,3 +1,12 @@
 package io.cat.ai.app
 
-trait TreeExApp[F[_], Input, Args, Options, Graph, Processor]
+trait TreeExApp[F[_], Input, Args, Options, Graph, Processor] {
+
+  def parseArgs(args: Input): F[Args]
+
+  def buildOptions(args: Args): F[Options]
+
+  def createGraph(options: Options): F[Graph]
+
+  def createProcessor(options: Options): F[Processor]
+}
